2730625e4d137c67d0bb124efb8c1b851d29f698
Weslley S. Patrocinio <wespatrocino@gmail.com>
2017-09-11 17:31:59 -0300

    Removing *.pyc files and updating notebook

--- music_genre_classification/app.py
+++ music_genre_classification/app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask
 from flask_restplus import Api
 from flask_restplus import fields
+from flask_restplus import Resource
 
 from pandas import DataFrame
 from sklearn.externals import joblib
@@ -12,33 +12,29 @@ from text_utils import text_tokenizer
 
 import pickle
 
+# API settings
 app = Flask(__name__)
-
 api = Api(
-   app,
-   version='1.0',
-   title='Music Genre Prediction API',
-   description='An API which predicts the genre of a music based on its lyrics.')
+    app,
+    title='Music Genre Prediction API',
+    description='An API which predicts the genre of a music based on its lyrics.',
+    version='1.0'
+)
 
-ns = api.namespace('genre_precition',
-   description='Predict music genre')
+ns = api.namespace('genre_precition', description='Predict music genre')
 
 # Parser
 parser = api.parser()
-
 parser.add_argument(
-  'SongLyrics',
-  type=unicode,
-  required=True,
-  help='Full lyrics of the song in raw format, without any text treatment.',
-  location='form')
+    'SongLyrics',
+    type=unicode,
+    required=True,
+    help='Full lyrics of the song in raw format, without any text treatment.',
+    location='form'
+)
 
 # web app
-resource_fields = api.model('Resource', {
-    'result': fields.String,
-})
-
-from flask.ext.restplus import Resource
+resource_fields = api.model('Resource', {'result': fields.String})
 @ns.route('/')
 class MusicGenreApi(Resource):
 
@@ -52,16 +48,12 @@ class MusicGenreApi(Resource):
 
     def get_result(self, args):
         lyrics = args["SongLyrics"]
-
         with open('../model/vectorizer.pkl', 'rb') as f:
             tfidf = pickle.load(f)
         with open('../model/classifier.pkl', 'rb') as f:
             classifier = pickle.load(f)
-
-        print tfidf.transform([lyrics])
-
         X = DataFrame(tfidf.transform([lyrics]).toarray())
-        return { "result": classifier.predict(X) }
+        return {"result": classifier.predict(X)}
 
 if __name__ == '__main__':
     app.run(debug=True)
